543fb023cc260781e28488366a8bd8e5fdce85db
Dan Mindru <mindrudan@gmail.com>
2015-10-19 01:40:49 +0200

    [#1] Remove iOS blue link for the phone number (close)

--- tasks/build.js
+++ tasks/build.js
@@ -15,7 +15,7 @@ function buildTask(options){
           name: 'Lars Normark Holmgaard',
           welcome: 'Hi there,',
           introParagraph: 'Thanks for writing up this email.<br/> We are delighted to reply with a custom footer template.',
-          contactMain: 'Call 81100200 or email us at',
+          contactMain: 'Call <a href="tel:81100200"><span>81100200</span></a> or email us at', // Wrap phone numbers like this to prevent iOS mail from making them blue.
           contactMail: 'info@fadeit.dk',
           contactSecondary: 'Anelystparken 31D&nbsp;&nbsp;&#9679;&nbsp;&nbsp;DK-8381 Tilst&nbsp;&nbsp;&#9679;&nbsp;&nbsp;Aarhus<br />',
           logoUrl: 'http://fadeit.dk/!email/fadeit-logo.png',
@@ -26,7 +26,8 @@ function buildTask(options){
       .pipe(inlineCss({
         applyTableAttributes: true,
         applyWidthAttributes: true,
-        preserveMediaQueries: true
+        preserveMediaQueries: true,
+        removeStyleTags: false
       }))
       .pipe(minifyHTML({quotes: true}))
       .pipe(gulp.dest('./dist/'))
