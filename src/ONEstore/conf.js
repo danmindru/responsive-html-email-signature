3b353daa1021306ec3be839e858b9aedc2c796fd
Jude Park <dusskapark@gmail.com>
2016-04-12 12:33:04 +0900

    ONEstore 메일 템플릿 추가

---
+++ src/ONEstore/conf.js
@@ -0,0 +1,19 @@
+/*
+ * NB: for images, if you pass in a relative path ('assets/logo.png'), they will be automatically embedded (base64). If you pass in a URL, it will stay as it is.
+ */
+
+module.exports = [
+  {
+    id: 'ONEstore', // Will be appended to the built templates.
+    signature: '서비스 기획팀 | 매니저',
+    name: '박 주 형',
+    welcome: '안녕하세요. 박주형 매니저 입니다.',
+    introParagraph: 'Thanks for writing up this email.<br/> We are delighted to reply with a responsive template.',
+    contactMain: '<a href="tel:+821021290021"><span>+82-10-2129-0021</span></a> | ', // Wrap phone numbers like this to prevent iOS mail from making them blue.
+    contactMail: 'jude.park@sk.com',
+    contactSecondary: '성남시 분당구 판교역로 188 SK플래닛 건물 11층',
+    logoUrl: 'http://cfile25.uf.tistory.com/image/2455244C570C6A371D53B6',
+    logoAlt: 'Onestore logo',
+    website: 'http://onesto.re'
+  }
+]
---
+++ src/ONEstoreBlack/conf.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    id: 'ONEstoreBlack',
+    signature: 'Best regards,',
+    name: 'Joo Hyung Park | Sevice Planning Team',
+    contactMain: 'Call <a href="tel:+821021290021"><span>+82-10-2129-0021</span></a> or email us at', // Wrap phone numbers like this to prevent iOS mail from making them blue.
+    contactMail: 'jude.park@sk.com',
+    slogan: 'The 1st integrated Android App store in Korea',
+    contactSecondary: '188, Pangyoyeok-ro, Bundang-gu, Seongnam-si, Gyeonggi-do, Korea',
+    logoUrl: 'http://cfile9.uf.tistory.com/image/2178343D570C68482371D5',
+    logoAlt: 'ONEstore. logo',
+    website: 'http://onesto.re/'
+  }
+]
