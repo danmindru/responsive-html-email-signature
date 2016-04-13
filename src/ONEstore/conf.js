5ffb911328fd69f93858b5b0c8e8a05ad84233f2
Jude Park <dusskapark@gmail.com>
2016-04-13 18:30:16 +0900

    ONE스토어 템플릿 추가

--- src/ONEstore/conf.js
+++ src/ONEstore/conf.js
@@ -5,15 +5,17 @@
 module.exports = [
   {
     id: 'ONEstore', // Will be appended to the built templates.
-    signature: '서비스 기획팀 | 매니저',
-    name: '박 주 형',
-    welcome: '안녕하세요. 박주형 매니저 입니다.',
+    // signature: '감사합니다.',
+    name: '매 니 저',
+    team: '스토어 기술개발팀',
+    title: 'SW engineer',
+    welcome: '안녕하세요.',
     introParagraph: 'Thanks for writing up this email.<br/> We are delighted to reply with a responsive template.',
-    contactMain: '<a href="tel:+821021290021"><span>+82-10-2129-0021</span></a> | ', // Wrap phone numbers like this to prevent iOS mail from making them blue.
-    contactMail: 'jude.park@sk.com',
+    contactMain: '<a href="tel:+821012345678"><span>+82-10-1234-5678</span></a> | ', // Wrap phone numbers like this to prevent iOS mail from making them blue.
+    contactMail: 'devhelper@onestore.co.kr',
     contactSecondary: '성남시 분당구 판교역로 188 SK플래닛 건물 11층',
-    logoUrl: 'http://cfile25.uf.tistory.com/image/2455244C570C6A371D53B6',
+    logoUrl: 'http://cfile24.uf.tistory.com/image/224EEA3B570DEAB015AB12',
     logoAlt: 'Onestore logo',
-    website: 'http://onesto.re'
+    website: 'http://onesto.re/'
   }
 ]
--- src/ONEstoreBlack/conf.js
+++ src/ONEstoreBlack/conf.js
@@ -1,13 +1,21 @@
+/*
+ * NB: for images, if you pass in a relative path ('assets/logo.png'), they will be automatically embedded (base64). If you pass in a URL, it will stay as it is.
+ */
+
 module.exports = [
   {
-    id: 'ONEstoreBlack',
+    id: 'ONEstoreBlack', // Will be appended to the built templates.
     signature: 'Best regards,',
-    name: 'Joo Hyung Park | Sevice Planning Team',
-    contactMain: 'Call <a href="tel:+821021290021"><span>+82-10-2129-0021</span></a> or email us at', // Wrap phone numbers like this to prevent iOS mail from making them blue.
-    contactMail: 'jude.park@sk.com',
-    slogan: 'The 1st integrated Android App store in Korea',
+    name: 'Joo Hyung Park',
+    team: 'Store Platform Developmet team',
+    title: 'SW engineer',
+    welcome: '안녕하세요.',
+    introParagraph: 'Thanks for writing up this email.<br/> We are delighted to reply with a responsive template.',
+    contactMain: 'Call <a href="tel:+821012345678"><span>+82-10-1234-5678</span></a> or email us at', // Wrap phone numbers like this to prevent iOS mail from making them blue.
+    contactMail: 'devhelper@onestore.co.kr',
+    // slogan: '\"The 1st integrated Android App store in Korea\"',
     contactSecondary: '188, Pangyoyeok-ro, Bundang-gu, Seongnam-si, Gyeonggi-do, Korea',
-    logoUrl: 'http://cfile9.uf.tistory.com/image/2178343D570C68482371D5',
+    logoUrl: 'http://cfile5.uf.tistory.com/image/21450F3B570DEAB01F8E92',
     logoAlt: 'ONEstore. logo',
     website: 'http://onesto.re/'
   }
