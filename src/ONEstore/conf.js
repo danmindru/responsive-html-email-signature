235ea51fc4458dd17d286512476d83d068b65b65
Jude Park <dusskapark@gmail.com>
2016-04-14 13:56:11 +0900

    ONE스토어 명함 규격 업데이트

--- src/ONEstore/conf.js
+++ src/ONEstore/conf.js
@@ -6,15 +6,15 @@ module.exports = [
   {
     id: 'ONEstore', // Will be appended to the built templates.
     // signature: '감사합니다.',
-    name: '매 니 저',
-    team: '스토어 기술개발팀',
-    title: 'SW engineer',
+    name: '0 0 0 매니저',
+    team: '스토어 기획팀',
+    title: 'Dev Relations',
     welcome: '안녕하세요.',
     introParagraph: 'Thanks for writing up this email.<br/> We are delighted to reply with a responsive template.',
     contactMain: '<a href="tel:+821012345678"><span>+82-10-1234-5678</span></a> | ', // Wrap phone numbers like this to prevent iOS mail from making them blue.
     contactMail: 'devhelper@onestore.co.kr',
     contactSecondary: '성남시 분당구 판교역로 188 SK플래닛 건물 11층',
-    logoUrl: 'http://cfile24.uf.tistory.com/image/224EEA3B570DEAB015AB12',
+    logoUrl: 'http://cfile25.uf.tistory.com/image/23388A45570F0087013014', // 티스토리 블로그 활용
     logoAlt: 'Onestore logo',
     website: 'http://onesto.re/'
   }
--- src/ONEstoreBlack/conf.js
+++ src/ONEstoreBlack/conf.js
@@ -7,15 +7,15 @@ module.exports = [
     id: 'ONEstoreBlack', // Will be appended to the built templates.
     signature: 'Best regards,',
     name: 'Joo Hyung Park',
-    team: 'Store Platform Developmet team',
-    title: 'SW engineer',
+    team: 'Service Planning Team',
+    title: 'Dev Relations',
     welcome: '안녕하세요.',
     introParagraph: 'Thanks for writing up this email.<br/> We are delighted to reply with a responsive template.',
     contactMain: 'Call <a href="tel:+821012345678"><span>+82-10-1234-5678</span></a> or email us at', // Wrap phone numbers like this to prevent iOS mail from making them blue.
     contactMail: 'devhelper@onestore.co.kr',
     // slogan: '\"The 1st integrated Android App store in Korea\"',
     contactSecondary: '188, Pangyoyeok-ro, Bundang-gu, Seongnam-si, Gyeonggi-do, Korea',
-    logoUrl: 'http://cfile5.uf.tistory.com/image/21450F3B570DEAB01F8E92',
+    logoUrl: 'http://cfile9.uf.tistory.com/image/24337545570F008706369D', // 티스토리 블로그 활
     logoAlt: 'ONEstore. logo',
     website: 'http://onesto.re/'
   }
