c09a5a82d3aef56bce85f45563e62597fd84b2e4
Dan Mindru <mindrudan@gmail.com>
2016-05-12 21:25:21 +0200

    More generic dark & light themes #12

--- src/dark/conf.js
+++ src/dark/conf.js
@@ -1,13 +1,21 @@
+/**
+ * @notes
+ *
+ * - for images, if you pass in a relative path ('assets/logo.png'), they will be automatically embedded (base64). If you pass in a URL, it will stay as it is.
+ * - the 'id' will be appended to the built template names, e.g. '/dist/signature-dark.html'
+ * - wrap phone numbers like this to prevent iOS mail from making them blue, e.g. `<a href="tel:004580100100"><span>(45) 80100100</span></a>`
+ *
+ */
 module.exports = [
   {
-    id: 'play',
+    id: 'dark',
     signature: 'Best regards,',
-    name: 'Jane Whatsmyname',
-    contactMain: 'Call <a href="tel:004581100200"><span>(45) 81100200</span></a> or email us at', // Wrap phone numbers like this to prevent iOS mail from making them blue.
-    contactMail: 'info@tryplay.dk',
+    name: 'The dark mail team',
+    contactMain: 'Call <a href="tel:004580100100"><span>(45) 80100100</span></a> or email us at',
+    contactMail: 'info@dark.dk',
     slogan: 'LED Pylon. LED Wall. Digital Signage.',
-    logoUrl: 'https://informationscreen.com/manage/assets/images/play-logo.png',
-    logoAlt: 'Play. logo',
-    website: 'http://tryplay.dk'
+    logoUrl: '/assets/dark.png',
+    logoAlt: 'dark logo',
+    website: 'http://dark.dk'
   }
 ];
--- src/light/conf.js
+++ src/light/conf.js
@@ -1,19 +1,24 @@
-/*
- * NB: for images, if you pass in a relative path ('assets/logo.png'), they will be automatically embedded (base64). If you pass in a URL, it will stay as it is.
+/**
+ * @notes
+ *
+ * - for images, if you pass in a relative path ('assets/logo.png'), they will be automatically embedded (base64). If you pass in a URL, it will stay as it is.
+ * - the 'id' will be appended to the built template names, e.g. '/dist/signature-light.html'
+ * - wrap phone numbers like this to prevent iOS mail from making them blue, e.g. `<a href="tel:004580100100"><span>(45) 80100100</span></a>`
+ *
  */
 
 module.exports = [
   {
-    id: 'fadeit', // Will be appended to the built templates.
+    id: 'light',
     signature: 'Yours truly,',
-    name: 'Jane Whatsmyname',
+    name: 'The light mail team',
     welcome: 'Hi there,',
     introParagraph: 'Thanks for writing up this email.<br/> We are delighted to reply with a responsive template.',
-    contactMain: 'Call <a href="tel:81100200"><span>81100200</span></a> or email us at', // Wrap phone numbers like this to prevent iOS mail from making them blue.
-    contactMail: 'info@fadeit.dk',
-    contactSecondary: 'Anelystparken 31, DK-8381 Tilst, Aarhus',
-    logoUrl: 'http://fadeit.dk/src/assets/img/brand/fadeit-logo.png',
-    logoAlt: 'fadeit logo',
-    website: 'http://fadeit.dk'
+    contactMain: 'Call <a href="tel:80100100"><span>80100100</span></a> or email us at',
+    contactMail: 'info@light.dk',
+    contactSecondary: 'Happy Steet 31, DK-8000 Aarhus C, Denmark',
+    logoUrl: '/assets/light.png',
+    logoAlt: 'light logo',
+    website: 'http://light.dk'
   }
 ];
