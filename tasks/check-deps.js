57b3e9524b1a83025f4982c64ba77bc29ab05cba
Dan Mindru <mindrudan@gmail.com>
2016-05-12 19:02:27 +0200

    Use gulp-david to check deps

---
+++ tasks/check-deps.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var gulp = require('gulp'),
+    david = require('gulp-david');
+
+function checkDepsTask(){
+  gulp.task('check-deps', function checkDeps(){
+    gulp
+      .src('package.json')
+      .pipe(david());
+  });
+}
+
+module.exports = checkDepsTask;
