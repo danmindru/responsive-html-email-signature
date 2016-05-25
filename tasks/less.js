4186ff05c80c7206d2e83dc1de6eac235135fe8e
Dan Mindru <mindrudan@gmail.com>
2016-05-25 20:45:08 +0200

    ✅ LESS support

--- tasks/less.js
+++ tasks/less.js
@@ -1,10 +1,19 @@
 'use strict';
 
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    less = require('gulp-less'),
+    autoprefixer = require('gulp-autoprefixer'),
+    rename = require('gulp-rename');
 
 function lessTask(options){
-  gulp.task('less', function(){
-    //todo
+  gulp.task('less', ['dupe'], function(){
+    return gulp.src(options.workingDir + '/**/*.less')
+      .pipe(less())
+      .pipe(autoprefixer({
+        browsers: ['last 5 versions']
+      }))
+      .pipe(rename({ extname: '.css' }))
+      .pipe(gulp.dest(options.workingDir));
   });
 }
 
