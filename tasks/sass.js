68643a7b9324175070dbd7dbf43720e26583c8d3
Dan Mindru <mindrudan@gmail.com>
2016-05-25 22:30:33 +0200

    🖌 SCSS ready to go, closes #11

--- tasks/sass.js
+++ tasks/sass.js
@@ -1,10 +1,19 @@
 'use strict';
 
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    autoprefixer = require('gulp-autoprefixer'),
+    sass = require('gulp-sass'),
+    rename = require('gulp-rename');
 
 function sassTask(options){
-  gulp.task('sass', function(){
-    //todo
+  gulp.task('sass', ['dupe'], function(){
+    return gulp.src(options.workingDir + '/**/*.scss')
+      .pipe(sass())
+      .pipe(autoprefixer({
+        browsers: ['last 5 versions']
+      }))
+      .pipe(rename({ extname: '.css' }))
+      .pipe(gulp.dest(options.workingDir));
   });
 }
 
