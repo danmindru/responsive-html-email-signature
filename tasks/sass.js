3c47d1f4bc4475b392db606f6a03c91a334c3b46
Dan Mindru <mindrudan@gmail.com>
2019-08-31 16:54:40 +0200

    Update sass task

--- tasks/sass.js
+++ tasks/sass.js
@@ -1,21 +1,25 @@
 'use strict';
 
-var gulp = require('gulp'),
-    autoprefixer = require('gulp-autoprefixer'),
-    sass = require('gulp-sass'),
-    rename = require('gulp-rename');
+const gulp = require('gulp'),
+  autoprefixer = require('gulp-autoprefixer'),
+  sass = require('gulp-sass'),
+  rename = require('gulp-rename');
 
-function sassTask(options){
-  gulp.task('sass', ['dupe'], function(){
-    return options
-      .src(options.workingDir + '/**/*.scss')
-      .pipe(sass())
-      .pipe(autoprefixer({
-        browsers: ['last 5 versions']
-      }))
-      .pipe(rename({ extname: '.css' }))
-      .pipe(gulp.dest(options.workingDir));
-  });
+function sassTask(options) {
+  // Requires: dupe.
+  gulp.task(
+    'sass',
+    gulp.series('dupe', function() {
+      return options
+        .src(options.workingDir + '/**/*.scss')
+        .pipe(sass())
+        .pipe(
+          autoprefixer()
+        )
+        .pipe(rename({ extname: '.css' }))
+        .pipe(gulp.dest(options.workingDir));
+    })
+  );
 }
 
 module.exports = sassTask;
