268cbed28274916471d7fe565b710071d9bebe3a
Dan Mindru <mindrudan@gmail.com>
2019-08-31 16:54:51 +0200

    Update lint task

--- tasks/lint.js
+++ tasks/lint.js
@@ -1,15 +1,19 @@
 'use strict';
 
-var gulp = require('gulp'),
+const gulp = require('gulp'),
     jsonlint = require("gulp-jsonlint");
 
 function lintTask(options){
-  gulp.task('lint', ['dupe'], function(){
-    return options
-      .src(options.workingDir + '/**/conf.json')
-      .pipe(jsonlint())
-      .pipe(jsonlint.reporter());
-  });
+  // Requiers: dupe.
+  gulp.task(
+    'lint',
+    function() {
+      return options
+        .src(options.workingDir + '/**/conf.json')
+        .pipe(jsonlint())
+        .pipe(jsonlint.reporter());
+    }
+  );
 }
 
 module.exports = lintTask;
